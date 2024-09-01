0677205d70086c2abeba88c6a65b05c25895799c
Felipe Noronha <felipenoris@gmail.com>
2024-08-31 21:47:54 -0300

    fix docs build

--- docs/make.jl
+++ docs/make.jl
@@ -4,8 +4,11 @@ using Documenter, BLPData
 makedocs(
     sitename = "BLPData.jl",
     modules = [ BLPData ],
-    pages = [ "Home" => "index.md",
-              "API Reference" => "api.md" ]
+    pages = [
+        "Home" => "index.md",
+        "API Reference" => "api.md",
+    ],
+    checkdocs=:none,
 )
 
 deploydocs(
